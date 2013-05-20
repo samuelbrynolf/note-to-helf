9751d770275b856104f7ce9270585ab5737caecb
Samuel Larsson <samuel.larsson@soderhavet.com>
2013-05-20 18:10:19 +0200

    ny minicopy listningsvy

---
+++ parts/listitem-link.php
@@ -0,0 +1,16 @@
+<div class="l-container m-listitem<?php if ( has_post_thumbnail() ) { ?> has-thumb<?php } ?>">
+	<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute( array( 'before' => 'L&auml;nk till: ', 'after' => '' ) ); ?>">
+		<section class="l-span-S12">
+			<?php if ( has_post_thumbnail() ) { ?>
+				<figure>
+					<?php the_post_thumbnail('large', array('class' => 'm-listitem-thumbnail')); ?>
+				</figure>
+			<?php } ?>
+			<h2 class="t-medium">Hittad &amp; gillad: <?php the_title(); ?></h2>
+			<p class="t-small item-type link">
+				<?php echo 'Delad den '.get_the_time('j F Y').'<br/>';
+		  	echo strip_tags(get_the_tag_list('',' + ','')); ?>
+			</p>
+		</section>
+	</a><?php // end .l-span-S12 ?>
+</div>
--- parts/listitem-log.php
+++ parts/listitem-log.php
@@ -1,3 +1,5 @@
+<?php $category = get_the_category(); ?>
+
 <div class="l-container m-listitem<?php if ( has_post_thumbnail() ) { ?> has-thumb<?php } ?>">
 	<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute( array( 'before' => 'L&auml;nk till: ', 'after' => '' ) ); ?>">
 		<section class="l-span-S12">
@@ -6,11 +8,12 @@
 					<?php the_post_thumbnail('large', array('class' => 'm-listitem-thumbnail')); ?>
 				</figure>
 			<?php } ?>
-			<h2 class="t-medium"><?php the_title(); ?></h2>
-			<p <?php post_class(); ?>>
-				<?php $category = get_the_category();
-				echo $category[0]->cat_name.': '.get_the_time('j F, Y').'<br/>';
-		  	echo strip_tags(get_the_tag_list('',' + ','')); ?>
+			<h2 class="t-medium"><?php echo $category[0]->cat_name.': &#8220;'.get_the_title(); ?>&#8221;</h2>
+			<p class="t-small item-type log">
+				<?php echo 'Projektlogg: '.get_the_time('j F Y').'<br/>';
+				if(has_tag()){
+		  	echo strip_tags(get_the_tag_list('',' + ','')); 
+		  	}; ?>
 			</p>
 		</section>
 	</a><?php // end .l-span-S12 ?>
