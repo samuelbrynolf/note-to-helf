167e3eccb07c1082c4a27912758112921379f358
Samuel Larsson <samuel.larsson@soderhavet.com>
2013-05-28 09:12:24 +0200

    fixar bilderna i listningen

--- parts/listitem-default.php
+++ parts/listitem-default.php
@@ -1,19 +1,38 @@
-<div class="l-container m-listitem<?php if ( has_post_thumbnail() ) { ?> has-thumb<?php } ?>">
-	<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute( array( 'before' => 'L&auml;nk till: ', 'after' => '' ) ); ?>">
-		<section class="l-span-S12">
-			<?php if ( has_post_thumbnail() ) { ?>
-				<figure>
-					<?php the_post_thumbnail('large', array('class' => 'm-listitem-thumbnail')); ?>
-				</figure>
-			<?php } ?>
-			<h2 class="t-medium"><?php the_title(); ?></h2>
-			<p class="t-small">
-				<?php echo 'Publicerad den '.get_the_time('j F Y').'<br/>';
-		  	echo strip_tags(get_the_tag_list('',' + ',''));
-		  	if(!has_tag()) {
-		  		echo '<br />';
-		  	} ?>
-			</p>
-		</section>
-	</a><?php // end .l-span-S12 ?>
-</div>
+<?php if ( has_post_thumbnail() ) { ?>
+
+	<div class="m-listitem has-thumb">
+		<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute( array( 'before' => 'L&auml;nk till: ', 'after' => '' ) ); ?>">
+			<figure>
+				<?php the_post_thumbnail('large', array('class' => 'm-listitem-thumbnail')); ?>
+			</figure>
+			<section class="m-entry">
+				<h2 class="t-medium"><?php the_title(); ?></h2>
+				<p class="t-small">
+					<?php echo 'Publicerad den '.get_the_time('j F Y').'<br/>';
+			  	echo strip_tags(get_the_tag_list('',' + ',''));
+			  	if(!has_tag()) {
+			  		echo '<br />';
+			  	} ?>
+				</p>
+			</section>
+		</a>
+	</div>
+	
+<?php } else { ?>
+
+	<div class="l-container m-listitem">
+		<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute( array( 'before' => 'L&auml;nk till: ', 'after' => '' ) ); ?>">
+			<section class="l-span-S12 m-entry">
+				<h2 class="t-medium"><?php the_title(); ?></h2>
+				<p class="t-small">
+					<?php echo 'Publicerad den '.get_the_time('j F Y').'<br/>';
+		  		echo strip_tags(get_the_tag_list('',' + ',''));
+		  		if(!has_tag()) {
+		  			echo '<br />';
+		  		} ?>
+				</p>
+			</section>
+		</a>
+	</div>
+	
+<?php } ?>
--- parts/listitem-log.php
+++ parts/listitem-log.php
@@ -1,21 +1,38 @@
-<?php $category = get_the_category(); ?>
+<?php if ( has_post_thumbnail() ) { ?>
 
-<div class="l-container m-listitem<?php if ( has_post_thumbnail() ) { ?> has-thumb<?php } ?>">
-	<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute( array( 'before' => 'L&auml;nk till: ', 'after' => '' ) ); ?>">
-		<section class="l-span-S12">
-			<?php if ( has_post_thumbnail() ) { ?>
-				<figure>
-					<?php the_post_thumbnail('large', array('class' => 'm-listitem-thumbnail')); ?>
-				</figure>
-			<?php } ?>
-			<h2 class="t-medium"><?php echo $category[0]->cat_name.': &#8220;'.get_the_title(); ?>&#8221;</h2>
-			<p class="t-small item-type log">
-				<?php echo 'Projektlogg: '.get_the_time('j F Y').'<br/>';
-		  	echo strip_tags(get_the_tag_list('',' + ','')); 
-		  	if(!has_tag()) {
-		  		echo '<br />';
-		  	} ?>
-			</p>
-		</section>
-	</a><?php // end .l-span-S12 ?>
-</div>
+	<div class="m-listitem has-thumb">
+		<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute( array( 'before' => 'L&auml;nk till: ', 'after' => '' ) ); ?>">
+			<figure>
+				<?php the_post_thumbnail('large', array('class' => 'm-listitem-thumbnail')); ?>
+			</figure>
+			<section class="m-entry">
+				<h2 class="t-medium"><?php the_title(); ?></h2>
+				<p class="t-small">
+					<?php echo 'Projektlogg: '.get_the_time('j F Y').'<br/>';
+			  	echo strip_tags(get_the_tag_list('',' + ',''));
+			  	if(!has_tag()) {
+			  		echo '<br />';
+			  	} ?>
+				</p>
+			</section>
+		</a>
+	</div>
+	
+<?php } else { ?>
+
+	<div class="l-container m-listitem">
+		<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute( array( 'before' => 'L&auml;nk till: ', 'after' => '' ) ); ?>">
+			<section class="l-span-S12 m-entry">
+				<h2 class="t-medium"><?php the_title(); ?></h2>
+				<p class="t-small">
+					<?php echo 'Projektlogg: '.get_the_time('j F Y').'<br/>';
+		  		echo strip_tags(get_the_tag_list('',' + ',''));
+		  		if(!has_tag()) {
+		  			echo '<br />';
+		  		} ?>
+				</p>
+			</section>
+		</a>
+	</div>
+	
+<?php } ?>
